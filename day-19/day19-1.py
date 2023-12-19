a3ea5a05f838c004b934ab67a345b7ca1f6bb8f9
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2023-12-20 00:06:46 +0200

    Day 19 Part 1 solution

---
+++ day-19/day19-1.py
@@ -0,0 +1,103 @@
+import sys
+sys.path.insert(0, '..')
+
+import re
+import operator
+from typing import Dict, List, Tuple, Union
+
+from helpers import filemap
+
+
+use_test_data = False
+inp = filemap('input.txt', lambda p: p.split('\n'), sep='\n\n') if not use_test_data else [
+    [
+        'px{a<2006:qkq,m>2090:A,rfg}',
+        'pv{a>1716:R,A}',
+        'lnx{m>1548:A,A}',
+        'rfg{s<537:gd,x>2440:R,A}',
+        'qs{s>3448:A,lnx}',
+        'qkq{x<1416:A,crn}',
+        'crn{x>2662:A,R}',
+        'in{s<1351:px,qqz}',
+        'qqz{s>2770:qs,m<1801:hdj,R}',
+        'gd{a>3333:R,R}',
+        'hdj{m>838:A,pv}'
+    ],
+    [
+        '{x=787,m=2655,a=1222,s=2876}',
+        '{x=1679,m=44,a=2067,s=496}',
+        '{x=2036,m=264,a=79,s=2244}',
+        '{x=2461,m=1339,a=466,s=291}',
+        '{x=2127,m=1623,a=2188,s=1013}'
+    ]
+] # 19114
+
+
+OP_MAP = {
+    '<': operator.lt,
+    '>': operator.gt,
+    '=': operator.eq
+}
+
+
+def parse_workflows(workflows: List[str]) -> Dict[str, List[Union[Tuple[str], Tuple[str, str, int, str]]]]:
+    rules = dict()
+
+    for wf in workflows:
+        i = wf.index('{')
+        name = wf[:i]
+        rules[name] = []
+        for rule in wf[i+1:-1].split(','):
+            colon_i = rule.find(':')
+            if colon_i == -1:
+                rules[name].append((rule, ))
+            else:
+                r = rule[:colon_i]
+                dst = rule[colon_i+1:]
+                op_i = re.search(r'>|<|=', r).start()
+                part, val = r.split(r[op_i:op_i+1])
+                rules[name].append((part, r[op_i], int(val), dst)) # part.field, operator, comparison val, dest_workflow
+
+    return rules
+
+
+def parse_parts(parts: List[str]) -> List[Dict[str, int]]:
+    ratings = []
+
+    for part in parts:
+        p_r = (pr.split('=') for pr in part[1:-1].split(','))
+        p = dict([(k, int(v)) for k, v in p_r])
+        ratings.append(p)
+
+    return ratings
+
+
+def workflow(part: Dict[str, int], workflow: Dict[str, List[Union[Tuple[str], Tuple[str, str, int, str]]]]) -> bool:
+    destination = 'in'
+
+    while True:
+        rules = workflow[destination]
+
+        for rule in rules:
+            if len(rule) == 1:
+                if rule[0] == 'A':
+                    return True
+                elif rule[0] == 'R':
+                    return False
+                destination = rule[0]
+                break
+
+            elif OP_MAP[rule[1]](part[rule[0]], rule[2]):
+                if rule[3] == 'A':
+                    return True
+                elif rule[3] == 'R':
+                    return False
+
+                destination = rule[3]
+                break
+
+
+wflows = parse_workflows(inp[0])
+parts  = parse_parts(inp[1])
+
+print('Part 1:', sum(sum(part[c] for c in tuple('xmas')) for part in parts if workflow(part, wflows)))
